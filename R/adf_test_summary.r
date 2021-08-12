b779e69a276b2e375717e64d4832042e8950a74c
chaoguo14 <49734092+chaoguo14@users.noreply.github.com>
2021-08-12 12:31:06 -0400

    Update adf_test_summary.r

--- R/adf_test_summary.r
+++ R/adf_test_summary.r
@@ -5,9 +5,11 @@ adf_test_summary <- function(df, type = "none", p_select = "BIC") {
   type_explanation <- c("no intercept, no time trend",
                         "with intercept, no time trend",
                         "with intercept, with linear time trend")
+  urca_to_adftest <- c("nc", "ct", "c")
+  names(urca_to_adftest) <- c("none","drift","trend")
   names(type_explanation) <- c("none", "drift", "trend")
   
-  cat(paste("Augmented Unit-Root Test. Maximum Lag:", max_lag, "\n"))
+  cat(paste("Augmented Unit-Root Test.\n"))
   cat(paste("Type: ", type_explanation[type], "\n"))
   cat("p-value< 0.05 implies no unit root exists (i.e. stationary)\n")
   
@@ -18,7 +20,7 @@ adf_test_summary <- function(df, type = "none", p_select = "BIC") {
                             function(l) tryCatch(
                               fUnitRoots::adfTest(x,
                                                   lags = urca::ur.df(test_ts, type = type, selectlags = p_select)@lags,
-                                                  type = type)@test$p.value,
+                                                  type = urca_to_adftest(type))@test$p.value,
                               error = function(err) NA)
              )
     )
