5b4ba325b20e3ddc5bf60b1b658e4a9e3a665a19
chaoguo14 <49734092+chaoguo14@users.noreply.github.com>
2021-08-12 12:29:16 -0400

    Update adf_test_summary.r

--- R/adf_test_summary.r
+++ R/adf_test_summary.r
@@ -1,20 +1,26 @@
-adf_test_summary <- function(df, max_lag = 5, type = "nc") {
+adf_test_summary <- function(df, type = "none", p_select = "BIC") {
   #' Run Augmented Dickey--Fuller test for unit roots on all columns of a data frame.
+  #' @param type "none", "drift" (intercept), "trend" (intercept + linear time trend)
+  #' @param p_select How to determin number of lags to include in the ADF test regression? c("AIC", "BIC")
   type_explanation <- c("no intercept, no time trend",
                         "with intercept, no time trend",
-                        "with intercept, with time trend")
-  names(type_explanation) <- c("nc", "ct", "c")
+                        "with intercept, with linear time trend")
+  names(type_explanation) <- c("none", "drift", "trend")
+  
   cat(paste("Augmented Unit-Root Test. Maximum Lag:", max_lag, "\n"))
   cat(paste("Type: ", type_explanation[type], "\n"))
   cat("p-value< 0.05 implies no unit root exists (i.e. stationary)\n")
+  
   data.frame(
     lapply(df,
            FUN = function(x)
              purrr::map_dbl(1:max_lag,
                             function(l) tryCatch(
-                              fUnitRoots::adfTest(x, lags = l, type = type)@test$p.value,
+                              fUnitRoots::adfTest(x,
+                                                  lags = urca::ur.df(test_ts, type = type, selectlags = p_select)@lags,
+                                                  type = type)@test$p.value,
                               error = function(err) NA)
              )
-           )
     )
+  )
 }
