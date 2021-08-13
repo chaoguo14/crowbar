445e461e70704e45d436725d9f8204b35f33e883
Chao Guo <cguo127@outlook.com>
2021-08-13 15:30:56 -0400

    Created Roxygen2 doc for adf_test_summary()

--- R/adf_test_summary.r
+++ R/adf_test_summary.r
@@ -1,13 +1,30 @@
 adf_test_summary <- function(df, type = "none", p_select = "BIC") {
   #' Run Augmented Dickey--Fuller test for unit roots on all columns of a data frame.
-  #' @param type "none", "drift" (intercept), "trend" (intercept + linear time trend)
-  #' @param p_select How to determin number of lags to include in the ADF test regression? c("AIC", "BIC")
+  #'
+  #' @param type Type of test equation. Possible values: "none", "drift" (intercept), "trend" (intercept + linear time trend)
+  #' @param p_select Method for selecting number of lags to include in the test regression. Possible values: "AIC", "BIC"
+  #'
+  #' @return A data frame with p-values for each column
+  #'
+  #' @examples
+  #' # Example 1: We test it on iid white noises and a random walk.
+  #' # As expected, white_noise's p-value is smaller than 0.01, so it is stationary. But random walk is not.
+  #' set.seed(10)
+  #' df <- data.frame(white_noise = rnorm(100), random_walk = cumsum(rnorm(100)))
+  #' adf_test_summary(df)
+  #'
+  #' # Example 2: We test it on a "trend stationary" time series.
+  #' set.seed(10)
+  #' df <- data.frame(trend_s = 0.8 * seq(1:20) + rnorm(20))
+  #' adf_test_summary(df)  # p-value > 0.05, but we are not using the correct test
+  #' adf_test_summary(df, type = "trend")  # This returns p-value < 0.05
   type_explanation <- c("no intercept, no time trend",
                         "with intercept, no time trend",
                         "with intercept, with linear time trend")
-  urca_to_adftest <- c("nc", "ct", "c")
-  names(urca_to_adftest) <- c("none","drift","trend")
   names(type_explanation) <- c("none", "drift", "trend")
+
+  urca_to_adftest <- c("nc", "c", "ct")
+  names(urca_to_adftest) <- c("none","drift","trend")
   
   cat(paste("Augmented Unit-Root Test.\n"))
   cat(paste("Type: ", type_explanation[type], "\n"))
