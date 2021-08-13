96a99bb2878a8940d5e60c3723f2d35a61299090
Chao Guo <cguo127@outlook.com>
2021-08-13 16:37:38 -0400

    display all 3 types of test

--- R/adf_test_summary.r
+++ R/adf_test_summary.r
@@ -1,10 +1,9 @@
-adf_test_summary <- function(df, type = "none", p_select = "BIC") {
-  #' Run Augmented Dickey--Fuller test for unit roots on all columns of a data frame.
+adf_test_summary <- function(df, p_select = "BIC") {
+  #' Run ADF test for unit roots on all columns of a data frame.
   #'
-  #' @param type Type of test equation. Possible values: "none", "drift" (intercept), "trend" (intercept + linear time trend)
   #' @param p_select Method for selecting number of lags to include in the test regression. Possible values: "AIC", "BIC"
   #'
-  #' @return A data frame with p-values for each column
+  #' @return A data frame with 3 rows and ncol(df) columns.
   #'
   #' @examples
   #' # Example 1: We test it on iid white noises and a random walk.
@@ -16,8 +15,7 @@ adf_test_summary <- function(df, type = "none", p_select = "BIC") {
   #' # Example 2: We test it on a "trend stationary" time series.
   #' set.seed(10)
   #' df <- data.frame(trend_s = 0.8 * seq(1:20) + rnorm(20))
-  #' adf_test_summary(df)  # p-value > 0.05, but we are not using the correct test
-  #' adf_test_summary(df, type = "trend")  # This returns p-value < 0.05
+  #' adf_test_summary(df)
   type_explanation <- c("no intercept, no time trend",
                         "with intercept, no time trend",
                         "with intercept, with linear time trend")
@@ -27,13 +25,20 @@ adf_test_summary <- function(df, type = "none", p_select = "BIC") {
   names(urca_to_adftest) <- c("none","drift","trend")
   
   cat(paste("Augmented Unit-Root Test.\n"))
-  cat(paste("Type: ", type_explanation[type], "\n"))
-  cat("p-value< 0.05 implies no unit root exists (i.e. stationary)\n")
+  # cat(paste("Type: ", type_explanation[type], "\n"))
+  cat("  [1]. p-value < 0.05 implies no unit root exists (i.e. stationary).\n")
+  cat("  [2]. Use domain knowledge to determine if including intercept/trend is appropriate.\n\n")
   
   data.frame(
-    lapply(df, FUN = function(x) fUnitRoots::adfTest(x,
-                            lags = urca::ur.df(x, type = type, selectlags = p_select)@lags,
-                            type = urca_to_adftest[type])@test$p.value
+    lapply(df, FUN = function(x) c("None" = fUnitRoots::adfTest(x,
+                            lags = urca::ur.df(x, type = "none", selectlags = p_select)@lags,
+                            type = urca_to_adftest["none"])@test$p.value,
+                            "Intercept" = fUnitRoots::adfTest(x,
+                            lags = urca::ur.df(x, type = "drift", selectlags = p_select)@lags,
+                            type = urca_to_adftest["drift"])@test$p.value,
+                            "Trend" = fUnitRoots::adfTest(x,
+                            lags = urca::ur.df(x, type = "trend", selectlags = p_select)@lags,
+                            type = urca_to_adftest["trend"])@test$p.value)
     )
   )
 }
